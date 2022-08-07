f732e2774a43b6aed6aacd1926921760240b0dd6
abrar <abrarulh7@gmail.com>
2022-08-07 13:43:30 +0530

    stepform

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
-import './App.css';
-import Modal from './components/Modal';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import Modal from "./components/Modal";
 
 function App() {
-
-  const [openModal,setOpenModal] = useState(false)
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <div className="App">
-   <h1>Click button to open modal</h1>
-   <button onClick={() => setOpenModal(true)} className="openModalBtn">Open Modal</button>
-   {openModal && <Modal closeModal={setOpenModal}/>}
+      <h1>Click button to open modal</h1>
+      <button onClick={() => setOpenModal(true)} className="openModalBtn">
+        Open Modal
+      </button>
+      {openModal && <Modal closeModal={setOpenModal} />}
+
+      <Form />
     </div>
   );
 }
